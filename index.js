5ba64706effc49eaecd6421339850a2333c26e1f
Alejandro <sancha0701@gmail.com>
2022-08-08 20:33:27 -0400

    commit tree 810921a8e5cdb094dda49459be5f5559db07f164

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log('text');
+    let text = document.getElementById('text').innerHTML = "This is really cool!";
+});
+
+
+
+
+
+// "This is really cool!"
+//JavaScript is so cool. It lets me add text to my page programmatically
